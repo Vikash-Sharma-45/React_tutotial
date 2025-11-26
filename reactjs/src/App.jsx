2db789bd5f9e787d44a2a72ab530895ee911bc76
Diwakar Gupta <diwakargond923@gmail.com>
2025-11-26 22:19:23 +0530

    Second commit

--- reactjs/src/App.jsx
+++ reactjs/src/App.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>Hello, React!</h1>
+      <div className='card'>
+        <h1>Vikash Sharma</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure aliquid voluptatibus, excepturi vitae aspernatur id laudantium, quaerat earum alias obcaecati totam, ducimus possimus exercitationem repellat distinctio saepe? Numquam, assumenda ipsum.</p>
+
+      </div>
+      
     </div>
   )
 }
