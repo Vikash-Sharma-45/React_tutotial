0de60a80e85a65f3146fe616b86153cd988de39e
Vikash-Sharma-45 <vikashsharmavk58@gmail.com>
2026-02-10 23:43:48 +0530

    Learned Functions

---
+++ Functions/React-Functions/src/App.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const App = () => {
+  function btnClick(){
+    console.log("Button Clicked")
+  }
+  function inputChange(val){
+    console.log(val)
+
+  }
+
+  const pageScrolling = (val) => {
+    if(val > 0) {
+      console.log("Scrolling Down")
+    } else {
+      console.log("Scrolling Up")
+    }
+  }
+
+  return (
+    <div>
+      <h1>Hello, Vikash</h1>
+      <input 
+        onChange={function(elem) {
+          inputChange(elem.target.value)
+        }} 
+        type="text" 
+        placeholder='Enter Name' 
+      />
+      <div 
+      onMouseMove={(elem) => 
+      console.log(elem.clientX)} 
+      className='box'>
+
+      </div>
+      <div onWheel={function (elem) {
+        pageScrolling(elem.deltaY)
+      }}>
+      <div className="page1"></div>
+      <div className="page2"></div>
+      <div className="page3"></div>
+      </div>
+    </div>
+  )
+}
+
+export default App
