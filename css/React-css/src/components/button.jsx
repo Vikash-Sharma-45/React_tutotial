df25f9b7f5ceff9edc445275e8dcff9db579dd69
Vikash-Sharma-45 <vikashsharmavk58@gmail.com>
2026-02-13 21:51:47 +0530

    Css Commit

---
+++ css/React-css/src/components/button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/btn.css'
+
+const Button = () => {
+  return (
+    <div className='btn'>
+      this is button
+    </div>
+  )
+}
+
+export default Button
---
+++ css/React-css/src/components/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      this is a header
+    </div>
+  )
+}
+
+export default Header
