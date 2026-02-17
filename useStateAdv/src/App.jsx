6ce96854a318cea02a5e3f06af97207303fc5aea
Vikash-Sharma-45 <vikashsharmavk58@gmail.com>
2026-02-17 21:33:43 +0530

    useStateAdvance Final commit

--- useStateAdv/src/App.jsx
+++ useStateAdv/src/App.jsx
@@ -4,11 +4,30 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [num, setNum] = useState({user: "Chandu", age: 22})
+  const [arr, setArr] = useState([1, 2, 3, 4, 5])
+
+  const change = () => {
+    // const newNum = {...num}
+    // newNum.user = "Dhyan"
+    // newNum.age = 23
+    // setNum(newNum)
+
+    setNum(prev=>({...prev,age:23}))
+
+    // const newArr = [...arr]
+    // newArr.push(6)
+    // setArr(newArr)
+  }
 
   return (
     <div>
-      APP
+      <h1>{num.user}, {num.age}</h1>
+      <button onClick={change}>Change</button>
+      <br />
+
+      <h1>{arr}</h1>
+      <button onClick={change} >Change</button>
     </div>
   )
 }
