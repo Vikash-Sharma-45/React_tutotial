96e0dfb26f58d318c75325f45fc7bde7946743cc
Vikash-Sharma-45 <vikashsharmavk58@gmail.com>
2026-02-18 22:39:11 +0530

    Two Way Binging

---
+++ two-way-binding/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [title, setTitle] = useState("")
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log("Form Submitted")
+  }
+
+  return (
+    <div>
+      <form onSubmit={(e)=>{
+        submitHandler(e)
+      }}>
+        <input type="text" placeholder='Enter your name' />
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
