5563e0d67c50433440ac274c00b4aae5dc0e4fe8
Vikash-Sharma-45 <vikashsharmavk58@gmail.com>
2026-03-08 16:24:17 +0530

    Api with fecth method

--- localstorage/LocalStorage/src/App.jsx
+++ localstorage/LocalStorage/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 const App = () => {
+
+
   // localStorage.setItem('name', 'John Doe')
   // localStorage.setItem('age', '30')
   // sessionStorage.setItem('sessionName', 'Vikash Sharma')
@@ -9,19 +11,26 @@ const App = () => {
   // console.log(name, age) // Output: John Doe
   // localStorage.clear()
   // sessionStorage.clear()
-    const user = {
-      name : "Vikash Sharma",
-      age : 320,
-      city : "Lucknow"
-    }
 
-    localStorage.setItem('user', JSON.stringify(user))
-    localStorage.clear()
-    console.log(user)
+
+    // const user = {
+    //   name : "Vikash Sharma",
+    //   age : 320,
+    //   city : "Lucknow"
+    // }
+
+    // localStorage.setItem('user', JSON.stringify(user))
+    // localStorage.clear()
+    // console.log(user)
+
+    async function getData() {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+      console.log(response)
+    }
 
   return (
     <div>
-      App
+      <button onClick={getData}>Get data</button>
     </div>
   )
 }
