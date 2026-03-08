47e1b44c91d58233dbac57d9fddf64b393e858c9
Vikash-Sharma-45 <vikashsharmavk58@gmail.com>
2026-03-08 16:29:37 +0530

    Api calling

--- localstorage/LocalStorage/src/App.jsx
+++ localstorage/LocalStorage/src/App.jsx
@@ -35,6 +35,9 @@ const App = () => {
     const getData = async () => {
       const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
       console.log(response)
+
+      const data = await response.json()
+      console.log(data)
     }
 
 
