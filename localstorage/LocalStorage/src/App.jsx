edea71258edf902572ea9db1fef3708eea3ed527
Vikash-Sharma-45 <vikashsharmavk58@gmail.com>
2026-03-13 23:59:45 +0530

    Understanding API calling and fetchig

--- localstorage/LocalStorage/src/App.jsx
+++ localstorage/LocalStorage/src/App.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
 
 const App = () => {
+
+  const [data, setData] = useState([])
 
 
   // localStorage.setItem('name', 'John Doe')
@@ -32,18 +35,35 @@ const App = () => {
     
     // Api call using fetch using arrow function
 
+    // const getData = async () => {
+    //   const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    //   console.log(response)
+
+    //   const data = await response.json()
+    //   console.log(data)
+    // }
+
+    // Api call using axios
+
+
+
     const getData = async () => {
-      const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-      console.log(response)
-
-      const data = await response.json()
-      console.log(data)
+      const response = await axios.get('https://picsum.photos/v2/list')
+      setData(response.data)
     }
-
 
   return (
     <div>
       <button onClick={getData}>Get data</button>
+      <div>
+        {
+          data.map(function(elem, idx){
+            return <h3>Hello {elem.author}{idx}</h3>
+          })
+        }
+      </div>
+
+      
     </div>
   )
 }
