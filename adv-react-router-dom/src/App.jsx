1817dfc048f51058a16691d708245a8ec0ed63fc
Vikash-Sharma-45 <vikashsharmavk58@gmail.com>
2026-03-23 22:16:29 +0530

    Understanding the advance react router dom

---
+++ adv-react-router-dom/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
+import NotFound from './pages/NotFound'
+
+const App = () => {
+  return (
+    <div className='h-screen bg-black text-white'>
+      <Navbar />
+      
+      <Routes> 
+        <Route path="/" element={ <Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} /> 
+        <Route path="/product/" element={<Product />} />
+
+        <Route path="*" element={<NotFound />} />
+
+        
+      </ Routes>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ adv-react-router-dom/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='flex justify-between items-center absolute bottom-0 w-full bg-cyan-900 py-4 px-4'>
+      Footer
+    </div>
+  )
+}
+
+export default Footer
